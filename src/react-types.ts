9c8920ff0f10d9f2f9692bd68d136cfba8f92d3f
Eric Chernuka <ericchernuka@gmail.com>
2023-01-27 19:45:24 -0700

    update to types

--- src/react-types.ts
+++ src/react-types.ts
@@ -1,14 +1,9 @@
 type Merge<M, N> = N extends Record<string, unknown> ? M : Omit<M, keyof N> & N;
 
-export type PropGetter<P = Record<string, unknown>, R = DOMAttributes> = (
-  props?: Merge<DOMAttributes, P>,
-  ref?: React.Ref<any>
-) => R & React.RefAttributes<any>;
+export type HTMLProps<T = any> = React.HTMLAttributes<T> &
+  React.RefAttributes<T>;
 
-export type RequiredPropGetter<
-  P = Record<string, unknown>,
-  R = DOMAttributes
-> = (
-  props: Merge<DOMAttributes, P>,
-  ref?: React.Ref<any>
-) => R & React.RefAttributes<any>;
+export type PropGetter<T extends React.ElementType = any, P = {}> = (
+  props?: React.ComponentPropsWithoutRef<T> & P,
+  ref?: React.Ref<any> | React.RefObject<any>
+) => Merge<HTMLProps<T>, P>;
